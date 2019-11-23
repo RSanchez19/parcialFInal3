dcd57291de28bbc9b190e83f385ed21cd038cef6
RSanchez19 <00191018@uca.edu.sv>
2019-11-23 16:46:33 -0600

    Añadiendo diseño y guardadno datos en base

--- models/mobile_application.js
+++ models/mobile_application.js
@@ -1,12 +1,12 @@
- const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
 
-const mobile_application=  new Schema({
-    name: String,
-    os: String, 
-    programming_language: String,
-    type: String,
-    multi_platform: String
-})
+const mobile_schema =  new Schema({
+    app_name: {type: String, required: true},
+    app_os: {type: String, required: true}, 
+    app_language:  {type: String, required: true},
+    app_type: {type: String, required: true},
+    app_multi_plattform: {type: String, required: true}
+}); 
 
-module.exports = mongoose.model('aplicaciones_moviles', mobile_application);
+module.exports = mongoose.model('mobile_app', mobile_schema); 
--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,38 @@ const mobile_app = require('../models/mobile_application');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Parcial Final' });
+  res.render('index', { title: '¡Aplicaciones Moviles!' });
 });
 
+router.get('/', async(req,res) =>{
+  const newMobile_app = await mobile_app.find();
+  res.render('index',{
+    newMobile_app
+  });
+}); 
+
+
 router.post('/add', async (req,res) => {
-const app = new mobile_app(req.body);
-await app.save();
-res.send('recibido')
+const {app_name, app_os, app_language, app_type, app_multi_plattform} = req.body; 
+const errors = []
+if(!app_name){
+  errors.push({text: "ERROR. Ingrese el nombre de la aplicacion"})
+}
+if(!app_os){
+  errors.push({text: "ERROR. Especifique el sistema operativo"})
+}
+if(!app_language){
+  errors.push({text: "ERROR. Ingrese el lenguaje de la aplicacion"})
+}
+if(errors.length > 0){
+   res.render('index', {errors, app_name, app_os, app_language})
+}else{
+  const newMobile_app = new mobile_app({app_name, app_os, app_language,
+  app_type, app_multi_plattform});
+  await newMobile_app.save();
+  res.render('index');
+  }
 }) 
 
 module.exports = router;
+
